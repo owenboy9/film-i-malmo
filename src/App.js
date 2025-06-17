f5eddb6e59886939703ac64117cdb13ff5a6f184
Patrick-B-L <patrick.brandt.lu@outlook.com>
2025-06-17 09:25:57 +0200

    Added routing and setup for file structure.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/home'
+import Contact from './pages/contact'
+import About from './pages/about';
 import TestSupabase from './TestSupabase'
 
 function App() {
   return (
     <div className="App">
-      <TestSupabase />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> } />
+          <Route path="/contact" element={ <Contact /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/test" element={<TestSupabase />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+        <h1>Welcome to Film i Malmö</h1>
+        <p>Your go-to platform for all things cinema in Malmö.</p>
+        <p>Explore movie schedules, book tickets, and enjoy the latest films.</p>
+        <p>Stay tuned for updates and new features!</p>
+    </div>
+  )
+}
