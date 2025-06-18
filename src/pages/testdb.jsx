c3b3d77cceeba815492bb8976f203194ec32cb55
Patrick-B-L <patrick.brandt.lu@outlook.com>
2025-06-18 14:28:01 +0200

    Tested Generic CRUD.

--- src/pages/testdb.jsx
+++ src/pages/testdb.jsx
@@ -1,54 +1,53 @@
-import React from 'react'
-import DbInsert from '../components/dbInsert'
-import DbRead from '../components/dbRead'
-import DbUpdate from '../components/dbUpdate'
-import DbDelete from '../components/dbDelete'
 import DbGenericInsert from '../components/dbGenericInsert'
 import DbGenericRead from '../components/dbGenericRead'
 import DbGenericUpdate from '../components/dbGenericUpdate'
 import DbGenericDelete from '../components/dbGenericDelete'
 
 export default function TestDb() {
+  const movieCrudProps = {
+    table: 'movies',
+    fields: [
+      { name: 'title', type: 'text' },
+      { name: 'description', type: 'text' }
+    ]
+  };
+
   return (
     <div>TestDB
         <p>This page is for testing database connections and queries.</p>
         <p>It will be used to ensure that the database is functioning correctly.</p>
         <p>Stay tuned for updates and results from the tests!</p>
-        <DbRead />
-        <DbInsert />
-        <DbUpdate />
-        <DbDelete />
-              <h2>Testa generisk insert</h2>
-      <DbGenericInsert
-        table="movies"
-        fields={[
-          { name: 'title', type: 'text' },
-          { name: 'description', type: 'text' }
-        ]}
-      />
-              <h2>Testa generisk read</h2>
+      
+      <h2>Testa generisk read</h2>
+      <DbGenericRead {...movieCrudProps} />
+      <h2>Testa generisk insert</h2>
+      <DbGenericInsert {...movieCrudProps} />
+      <h2>Testa generisk update</h2>
+      <DbGenericUpdate {...movieCrudProps} />
+      <h2>Testa generisk delete</h2>
+      <DbGenericDelete {...movieCrudProps} />
+
+      <h2>Testa generisk insert collab</h2>
       <DbGenericRead
-        table="movies"
-        fields={[
-          { name: 'title', type: 'text' },
-          { name: 'description', type: 'text' }
-        ]}
+        table="collaborators"
+        fields={ [ {name:'name', type: 'text'},
+          {name:'partnership_type', type: 'partnership_type'}
+        ] }
       />
-              <h2>Testa generisk update</h2>
+      <h5>partnership_type: sponsor/collaborator</h5>
+      <DbGenericInsert
+        table="collaborators"
+        fields={ [ {name:'name', type: 'text'},
+          {name:'partnership_type', type: 'partnership_type'}
+        ] }
+      />
       <DbGenericUpdate
-        table="movies"
-        fields={[
-          { name: 'title', type: 'text' },
-          { name: 'description', type: 'text' }
-        ]}
+        table="collaborators"
+        fields={ [ {name:'name', type: 'text'}] }
       />
-              <h2>Testa generisk delete</h2>
       <DbGenericDelete
-        table="movies"
-        fields={[
-          { name: 'title', type: 'text' },
-          { name: 'description', type: 'text' }
-        ]}
+        table="collaborators"
+        fields={ [ {name:'name', type: 'text'}] }
       />
     </div>
   )
