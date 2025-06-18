2fa1f25315d0977987ddb1c17d0c4b08a61a3e88
David Hoang <62556785+Zereis@users.noreply.github.com>
2025-06-18 14:32:13 +0200

    features

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import logo from './logo.svg';
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router';
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/home'
 import Contact from './pages/contact'
 import About from './pages/about';
 import TestSupabase from './TestSupabase'
+import ResetPasswordPage from './pages/ResetPasswordPage'
+
+function HashRedirector() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (window.location.hash.includes('access_token')) {
+      const params = new URLSearchParams(window.location.hash.substring(1));
+      const type = params.get('type');
+      if (type === 'recovery') {
+        navigate('/reset-password' + window.location.hash, { replace: true });
+      }
+      // else: do nothing, let the user go to home or handle confirmation
+    }
+  }, [navigate]);
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <Home /> } />
+          <Route path="/" element={<><HashRedirector /><Home /></>} />
           <Route path="/contact" element={ <Contact /> } />
           <Route path="/about" element={ <About /> } />
           <Route path="/test" element={<TestSupabase />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
         </Routes>
       </BrowserRouter>
     </div>
