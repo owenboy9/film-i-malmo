aa5f99755785147cf94e9d41fd6484830d30f48c
Ferniie <theferniie@gmail.com>
2025-06-19 14:05:24 +0200

    la till sidor

---
+++ src/pages/annualmeeting.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function annualmeeting() {
+  return (
+    <div>Annual Meeting</div>
+  )
+}
---
+++ src/pages/currentpastprojects.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function currentpastprojects() {
+  return (
+    <div>current 6 past projects</div>
+  )
+}
