a10a9f32bd6936004ffcf2e1c9c884a86cb05d53
owenboy9 <owenkraus9@gmail.com>
2025-06-25 11:28:23 +0200

    simple schedule filter using generic crud props

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import DbGenericRead from '../components/DbGenericRead'
 import { crudConfigs } from '../components/GenericCrudProps'
 
-export default function schedule() {
+export default function Schedule() {
+  const { table, fields } = crudConfigs.events
+
   return (
-    <div>Schedule
-      <DbGenericRead table={crudConfigs.events} fields={crudConfigs.events} />
+    <div>
+      <h1>Schedule</h1>
+      <DbGenericRead
+        table={table}
+        fields={fields.filter(f => f.name === 'title' || f.name === 'starts_at')}
+        filter={(query) => query.gt('starts_at', new Date().toISOString())}
+      />
+
     </div>
   )
 }
