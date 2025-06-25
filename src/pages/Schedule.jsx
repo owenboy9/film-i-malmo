1a98ef3577a55b0a2fa876c0cb250526aca864ed
Patrick-B-L <patrick.brandt.lu@outlook.com>
2025-06-25 11:07:44 +0200

    Minor updates

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import DbGenericRead from '../components/DbGenericRead'
 
 export default function schedule() {
+  const aboutCrudProps = {
+    table: 'about',
+    fields: [
+      { name: 'title', type: 'text' },
+      { name: 'description', type: 'text' }
+    ],
+    optionLabels: ['title'],
+    filter: q => q.eq('title', 'film i malmö')
+  };
   return (
-    <div>Schedule</div>
+    <div>Schedule
+      < DbGenericRead {...aboutCrudProps} />
+    </div>
   )
 }
