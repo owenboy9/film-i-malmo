203345ce620338f3b872277c27c111a7bdfa2caa
Ferniie <theferniie@gmail.com>
2025-06-26 11:16:58 +0200

    more links

--- src/pages/More.jsx
+++ src/pages/More.jsx
@@ -7,27 +7,32 @@ export default function more() {
     <div className="more">
       <h1>More Links</h1>
           
-            <a href="/Press">Press</a> < br />
-            <a href="/Freescreen">Freescreen</a> < br />
-            <a href="/AnnualMeeting">Annual Meeting</a> < br />
-            <a href="/CurrentBoard">Current Board</a> < br />
-            <a href="/Hypnos">Cafe </a> < br />
-            <a href="https://hypnostheatre.com/" target="_blank">Hypnos </a> < br />
-            <a href="/PastEvents">Past Events</a> < br />
-            <a href="/CurrentPastProjects">Current & Past Projects</a> < br />
-            <a href="/Volunteering">Volunteering</a>
+          
+      
+      <a href="/Freescreen"><button>Freescreen</button></a> 
+      <a href="/PastEvents" ><button>Past Events</button></a> < br />
+      <a href="/CurrentBoard" ><button>Current Board</button></a> 
+      <a href="/Hypnos" ><button>Café</button></a> < br />
+      <a href="https://hypnostheatre.com/" target="_blank" ><button>Hypnos</button></a> 
+      <a href="/Volunteering" ><button>Volunteering</button></a>
+
+
+            {/* <a href="/Press" className="morebuttons">Press</a>  */}
+            {/* <a href="/AnnualMeeting" className="morebuttons">Annual Meeting</a> < br /> */}
+            {/* <a href="/CurrentPastProjects" className="morebuttons">Current & Past Projects</a>  */}
+           
 
             <hr/>
 
             <h2> Newsletter</h2>
             <form className="newsletter">
-            <input type="text" placeholder="Email"/><button type="submit">Sign up</button><br/>
+            <input type="text" placeholder="Email" /><button type="submit" >Sign up</button><br/>
             </form>
             
-            <p> logo folder</p> <br />
+            {/* <p> logo folder</p> <br /> */}
 
-            <a href="/MemberInfo">Member Info</a> <br />
-            <a href="/SinglePage">Single Page</a>
+            {/* <a href="/MemberInfo">Member Info</a> <br />
+            <a href="/SinglePage">Single Page</a> */}
 
 
     </div>
